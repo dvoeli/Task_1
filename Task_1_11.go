a87a8b027e388ebc64153c1fab1e6e7630f65bc6
Eli Dvoretsky <dvoretskyeli@icloud.com>
2021-10-28 17:31:03 +0200

    correction

--- Task_1_11.go
+++ Task_1_11.go
@@ -9,7 +9,7 @@ func main() {
 	var a float64
 	fmt.Scanln(&a)
 
-	z1 := math.Pow((1-2*math.Sin(a)), 2)/(1+math.Sin(2*a))
+	z1 := (1-2*math.Pow(math.Sin(a), 2))/(1+2*math.Sin(2*a))
 	z2 := (1-math.Tan(a))/(1+math.Tan(a))
 	fmt.Println(z1, z2)
 }
